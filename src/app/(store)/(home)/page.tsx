a89701b27450601f0e79cf107fd762bb9f385e27
Gabriel Alcântara de Paiva <gabriel.alcantara.ti@gmail.com>
2024-09-03 20:36:54 -0300

    Added Search Page

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -19,7 +19,7 @@ export default async function Home() {
     <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
       <Link
         href={`/product/${highlightedProduct.slug}`}
-        className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-start"
+        className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
       >
         <Image
           src={highlightedProduct.image}
@@ -29,7 +29,7 @@ export default async function Home() {
           quality={100}
           className="group-hover:scale-105 transition-transform duration-500"
         />
-        <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[268px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+        <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
           <span className="text-sm truncate">{highlightedProduct.title}</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
             {highlightedProduct.price.toLocaleString('pt-BR', {
@@ -45,7 +45,7 @@ export default async function Home() {
         <Link
           key={otherProduct.id}
           href={`/product/${otherProduct.slug}`}
-          className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-start"
+          className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
         >
           <Image
             src={otherProduct.image}
@@ -55,7 +55,7 @@ export default async function Home() {
             quality={100}
             className="group-hover:scale-105 transition-transform duration-500"
           />
-          <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[268px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
             <span className="text-sm truncate">{otherProduct.title}</span>
             <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
               {otherProduct.price.toLocaleString('pt-BR', {
